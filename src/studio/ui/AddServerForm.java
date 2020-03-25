d0071d940d7f42df47642257642658b5c9caea4a
dzmipt <dz.mipt@gmail.com>
2020-03-25 23:51:21 +0300

    Add Server List

--- src/studio/ui/AddServerForm.java
+++ src/studio/ui/AddServerForm.java
@@ -1,10 +1,11 @@
 package studio.ui;
 
+import studio.kdb.Server;
+
 import javax.swing.JFrame;
 
 public class AddServerForm extends ServerForm {
     public AddServerForm(JFrame owner) {
-        super(owner);
-        this.setTitle("Add a new server");
+        super(owner, "Add a new server", new Server());
     }
 }
